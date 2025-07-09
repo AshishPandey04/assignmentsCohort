6aeb53a8130c800c58020c263d3dbd528708677c
AshishPandey04 <0203ashish2004@gmail.com>
2025-07-10 00:54:40 +0530

    feat: add user authentication with signup and login functionality, including token generation and password hashing

--- week-4/hard/database/index.js
+++ week-4/hard/database/index.js
@@ -1,10 +1,7 @@
-
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');
-
-
+const bcrypt = require("bcrypt");
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL);
@@ -27,45 +24,76 @@ const UserSchema = new mongoose.Schema(
       required: true,
       lowercase: true,
       unique: true,
-      
+
       trim: true,
     },
     password: {
       type: String,
       required: true,
     },
+    refreshToken: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
 
 UserSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+  if (!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
-UserSchema.methods.isPasswordCorrect=async function (password) {
-  return await bcrypt.compare(password,this.password)
-  
-}
-const TodoSchema = new mongoose.Schema({
-  // Schema definition here
-  title: {
-    type: String,
-    required: true,
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const TodoSchema = new mongoose.Schema(
+  {
+    // Schema definition here
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
   },
-  description: {
-    type: String,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  category:{
-    type:String,
-    required:true
-  }
-},{timestamps:true});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 const Todo = mongoose.model("Todo", TodoSchema);
--- week-4/hard/routes/user.js
+++ week-4/hard/routes/user.js
@@ -1,109 +1,137 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-const {User}=require("../database/index")
+const { User } = require("../database/index");
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Something went wrong while generating Access Token and Refresh Token",
+    });
+  }
+};
 
 // User Routes
-router.post('/signup',async (req, res) => {
-    // Implement user signup logic
-    //get inputs from user
-    const {username,email,password}=req.body;
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  //get inputs from user
+  const { username, email, password } = req.body;
 
-    if(username==""){
-        return res.status(400).json({
-            msg:"Username is Required"
-        })
-    }
-    if(email==""){
-        return res.status(400).json({
-            msg:"email is Required"
-        })
-    }
-    if(password==""){
-        return res.status(400).json({
-            msg:"password is Required"
-        })
-    }
+  if (username == "") {
+    return res.status(400).json({
+      msg: "Username is Required",
+    });
+  }
+  if (email == "") {
+    return res.status(400).json({
+      msg: "email is Required",
+    });
+  }
+  if (password == "") {
+    return res.status(400).json({
+      msg: "password is Required",
+    });
+  }
 
-     const exitedUser=  await User.findOne({
-      $or: [{username},{email}]
-   })
-   if(exitedUser){
+  const exitedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (exitedUser) {
     return res.status(409).json({
-        msg:"Username or Email already exists"
-    })
-   }
+      msg: "Username or Email already exists",
+    });
+  }
 
-   const user = await User.create({
+  const user = await User.create({
     username,
     email,
-    password
-
-   })
-     const createdUser = await User.findById(user._id).select(
-      "-password "
-  )
-   if(!createdUser){
+    password,
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken "
+  );
+  if (!createdUser) {
     return res.status(500).json({
-        msg:"Something went wrong while registery the user"
-    })
-   }
-   return res.status(200).send(createdUser);
-   
-  
+      msg: "Something went wrong while registery the user",
+    });
+  }
+  return res.status(200).send(createdUser);
 });
 
-router.post('/login',async (req, res) => {
-     // Implement user login logic
+router.post("/login", async (req, res) => {
+  // Implement user login logic
 
+  // email and password
+  const { email, password } = req.body;
+  //check if email or password is empty
 
-     // email and password
-     const {email,password}=req.body;
-     //check if email or password is empty 
+  if (email === "") {
+    return res.status(400).json({
+      msg: "Email is required",
+    });
+  }
+  if (password === "") {
+    return res.status(400).json({
+      msg: "password is required",
+    });
+  }
+  const user = await User.findOne({
+    email: { $eq: email },
+  });
+  if (!user) {
+    return res.status(400).json({
+      msg: "User is not found , Sign Up",
+    });
+  }
 
-     if(email===""){
-        return res.status(400).json({
-            msg:"Email is required"
-        })
-     }
-     if(password===""){
-        return res.status(400).json({
-            msg:"password is required"
-        })
-     }
-     const user = await User.findOne({
-       email: { $eq: email } 
-     })
-     if(!user){
-        return res.status(400).json({
-            msg:"User is not found , Sign Up"
-        })
-     }
-     if(user.password!=password){
-        return res.status(401).json({
-            msg:"password is wrong"
-        })
+  const isPasswordValid = await user.isPasswordCorrect(password);
 
-     }
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      msg: "Password is Wrong",
+    });
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
 
-     const loggedInUser=await User.findById(user._id).select("-password")
-  return res.status(200).json({
-  msg: "User logged in successfully",
-  user: loggedInUser
-});
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
 
-    
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+
+    .json({
+      msg: "User logged in successfully",
+      user: loggedInUser,
+    });
 });
 
 // router.get('/todos', userMiddleware, (req, res) => {
-    // Implement logic for getting todos for a user
+// Implement logic for getting todos for a user
 
 // });
 
-router.post('/logout', userMiddleware, (req, res) => {
-    // Implement logout logic
+router.post("/logout", userMiddleware, (req, res) => {
+  // Implement logout logic
 
+  
 });
 
-module.exports = router
+module.exports = router;
