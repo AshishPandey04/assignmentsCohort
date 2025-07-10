9d3a220640cf08c5306a3630315a21a08a5f9421
AshishPandey04 <0203ashish2004@gmail.com>
2025-07-10 12:43:12 +0530

    feat: enhance user authentication middleware and integrate it into todo routes for secure access control

--- week-4/hard/middleware/user.js
+++ week-4/hard/middleware/user.js
@@ -1,6 +1,43 @@
-function userMiddleware(req, res, next) {
+const jwt = require("jsonwebtoken")
+const { User } = require("../database/index");
+
+
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     
+    // here we will verify is there is acesstoken and does  it match with the access token we have given while login the user 
+    //if both the conditions comes true that means user is login 
+    // or else logout out 
+
+  try {
+      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+    if(!token){
+        res.status(401).json({
+            msg:"Unauthorized token"
+        })
+    }
+
+    const decodedToken= jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -accessToken")
+
+    if(!user){
+        res.status(401).json({
+            msg:"Invalid Access Token"
+        })
+    }
+
+    req.user=user
+    next();
+  } catch (error) {
+    res.status(500).json({
+        msg:error?.message
+    })
+    
+  }
+
+
 }
 
 module.exports = userMiddleware;
--- week-4/hard/routes/todo.js
+++ week-4/hard/routes/todo.js
@@ -1,30 +1,112 @@
 const { Router } = require("express");
-const adminMiddleware = require("../middleware/user");
+const userMiddleware = require("../middleware/user");
+const {Todo}= require("../database/index")
+const { isValidObjectId } = require("mongoose")
 const router = Router();
 
 // todo Routes
-router.post('/', (req, res) => {
+router.post('/',userMiddleware ,async (req, res) => {
     // Implement todo creation logic
+    const {title,description,category}=req.body;
+    if(title || description||category){
+      return  res.status(400).json({
+            msg:"title , description and category is required"
+        })
+    }
+    const owner = req.user
+    if(!user){
+       return res.status(401)
+        .json({
+            msg:"Login to Add Todo"
+
+        })
+
+    }
+
+    const todo= await Todo.create({
+        title,
+        description,
+        category,
+        owner,
+    })
+    if(!todo){
+      return  res.status(500).json({
+            msg:"Something wen wrong while creating the todo"
+        })
+    }
+    return res.status(200).json({
+        msg:"todo Created successfully",
+        todo:todo
+
+    })
 });
 
-router.put('/', adminMiddleware, (req, res) => {
+router.put('/:id', userMiddleware, async (req, res) => {
     // Implement update todo  logic
+    const {todoId}=req.params
+    const {title,description,category}=req.body
+
+    if(!isValidObjectId(todoId)){
+        return res.status(400).json({
+            msg:"Invalid TodoId"
+        })
+    }
+    if(title ===""|| description===""|| category===""){
+        return res.status(401).json({
+            msg:"Title , description and category is required"
+        })
+    }
+
+    const todo = await Todo.findById(todoId)
+    if(!todo){
+
+        return res.status(404).json({
+            msg:"Todo not found"
+        })
+    }
+
+    const updatedTodo= await Todo.findByIdAndUpdate(
+        todoId,
+        {
+            $set:{
+                title,
+                description,
+                category
+            }
+
+        },{
+            new:true
+
+        }
+    )
+
+    if(!updatedTodo){
+        return res.status(500).json({
+            msg:"Something went wrong while updating the Todo"
+        })
+    }
+    return res.status(200).json({
+        msg:"Successfully Updated",
+        updatedTodo
+    })
+    
+
 });
 
-router.delete('/', adminMiddleware, (req, res) => {
+router.delete('/', userMiddleware, (req, res) => {
     // Implement delete todo logic
 });
 
-router.delete('/:id', adminMiddleware, (req, res) => {
+router.delete('/:id', userMiddleware, (req, res) => {
     // Implement delete todo by id logic
 });
 
 
-router.get('/', adminMiddleware, (req, res) => {
+router.get('/', userMiddleware, (req, res) => {
     // Implement fetching all todo logic
 });
 
-router.get('/:id', adminMiddleware, (req, res) => {
+router.get('/:id', userMiddleware, (req, res) => {
     // Implement fetching todo by id logic
 });
 
